ed748ec4e1be68f6c048f7f4d2e0489c4720abae
Agus Bes7 <aguscabj10.ab@gmail.com>
2025-01-13 05:16:55 -0300

    Consulta API mejoras(no agregar libros que ya existen,cubrir consultas no encontradas). Metodos de listas de autores(por fecha y el total) y libros(por idioma y listado) implementados resta conectar con la base de datos

--- src/main/java/LiterAlura/model/Clases/Libro.java
+++ src/main/java/LiterAlura/model/Clases/Libro.java
@@ -1,63 +1,41 @@
 package LiterAlura.model.Clases;
 
 import LiterAlura.model.Datos.DatosLibro;
+import LiterAlura.model.Idioma;
 
 public class Libro {
-    Integer id;
+    //Integer id;
     String titulo;
    Autor autoria;
-    String idiomas;
+    Idioma lenguaje;
+    Integer Descargas;
+
+//---------------------------------------------
     public  Libro(DatosLibro libro){
         this.titulo=libro.titulo();
         this.autoria=new Autor(libro.autor());
-        this.id=libro.id();
+       // this.id=libro.id();
         this.Descargas= libro.descargas();
-        this.idiomas=libro.idioma();
-    }
+        this.lenguaje=Idioma.fromString(libro.idioma());}
 
-    Integer Descargas;
-    public String getTitulo() {
-        return titulo;
-    }
+    public String getTitulo() {return titulo;}
+    public Idioma getLenguaje() {return lenguaje;}
+    public Integer getDescargas() {        return Descargas;}
+    public Autor getAutoria() {return autoria;}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getIdiomas() {
-        return idiomas;
-    }
-
-    public void setIdiomas(String idiomas) {
-        this.idiomas = idiomas;
-    }
-
-    public Integer getDescargas() {
-        return Descargas;
-    }
-
-    public void setDescargas(Integer descargas) {
-        Descargas = descargas;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public Autor getAutoria() {
-        return autoria;
-    }
+    public void setTitulo(String titulo) {this.titulo = titulo;    }
+    public void setLenguaje(Idioma lenguaje) {this.lenguaje = lenguaje;}
+    public void setDescargas(Integer descargas) {Descargas = descargas;}
 
     @Override
     public String toString() {
-
-
         return """
                 ╔==========================================╗
                 ..................Libro.....................
                 ╚==========================================╝
                    Titulo:        """+ titulo +'\n'+
                "   Autor:         " + autoria.getNombre()+'\n' +
-               "   Idioma:        " + idiomas + '\n' +
+               "   Idioma:        " + lenguaje + '\n' +
                "   Descargas:     " + Descargas +'\n' +
                """
                 ============================================
